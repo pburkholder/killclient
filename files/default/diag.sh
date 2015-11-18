714045c6a7d4245fc8c0e45a726b84e23e6a9bfb
Peter Burkholder <pburkholder@chef.io>
2015-11-18 11:00:42 -0500

    Fix LEs, and awk

--- files/default/diag.sh
+++ files/default/diag.sh
@@ -1,11 +1,27 @@
 #!/bin/bash -xe
 
+# If a chef-client is wedged, run this script to:
+# - capture diagnostics from running process
+# - drop a ping on the chef-server
+# - kill all the chef chef-clients
+# - get the run-parameters for chef from Cron (if # applicable)
+# - rerun with strace and debugging on.
+# - bundle it all up.
+#
+# In addition to sending the .tgz off to a workstation
+# for analysis, you'd also want the access logs from
+# ChefServer. E.g.
+#   ip='1.2.3.4'
+# Get the 100 lines before wedging:
+#  awk "NR==1,/$ip.*wedged_process/ { if ( \$1 == \"$ip\" ) print; } " /var/log/opscode/nginx/access.log | tail -100
+# Get the lines between wedging and the debug run
+#   awk "/^$ip.*wedged/, /^$ip.*debug/ { if ( \$1 == \"$ip\" ) print; }" /var/log/opscode/nginx/access.log
+
+
 CACHE_PATH=$(knife exec \
   -c /etc/chef/client.rb \
   -E "print Chef::Config.cache_path")
 TMPDIR=$CACHE_PATH/tmp
-CACHEDIR=/var/chef/cache
-
 
 function die() {
   echo -n $@
@@ -24,10 +40,10 @@ function make_tempdir(){
    fi
 }
 
+# Get files held open but current chef-client
 function lsof_wedged_process() {
   process=$1
   f="$REPORT_DIR/lsof_wedged_process"
-  # What can we dig out of the currently-wedged chef process?
   touch $f ||
     die "Unable to make output file $f"
   cmd="lsof -p $process"
@@ -44,15 +60,21 @@ function ps_all_chef_processes() {
   $cmd >> $f || die "No processes found"
 }
 
+# write an access log entry for a non-existent
+# URL to the chef-server
 function ping_chef_server() {
-  knife raw -c /etc/chef/client.rb /ping/from/$REPORT_DIR/$1 || true
+  knife raw -c /etc/chef/client.rb /ping/$1/$REPORT_DIR || true
 }
 
+# set $chefclient to the chef-client command in cron
 function get_chef_client_cron_job() {
   chefclient=$(crontab -l | grep -v '^ *#' | grep chef-client |
     awk 'BEGIN{ORS=" ";} {for(i=6;i<=NF;++i) print $i}')
+  [ -z $chefclient ] && chefclient='chef-client'
+
 }
 
+# run chef-client from strace with debug logging
 function run_chef_client() {
   f=$REPORT_DIR/strace_cmd
   touch $f || die "Unable to make output file $f"
@@ -61,12 +83,14 @@ function run_chef_client() {
   eval $cmd 2>&1 1>> $f
 }
 
+# pack up everything from REPORT_DIR into .tgz
 function pack_up() {
   tar -C $CACHE_PATH -czf $REPORT_DIR.tgz $REPORT_DIR
   echo "Work complete and bundled in:"
   echo "$REPORT_DIR.tgz"
 }
 
+# MAIN
 make_tempdir
 chef_pid=`cat $CACHE_PATH/cache/chef-client-running.pid`
 ps_all_chef_processes
